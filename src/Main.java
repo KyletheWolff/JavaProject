0bcaa0e5aecb1fbefcd04b132277ac40e2337d3d
KyletheWolff <kyle.a.wolff87@gmail.com>
2015-02-27 00:19:33 -0500

    Added more code to the JFrame contractor and also added the EventQueue

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,37 @@
+import java.awt.EventQueue;
 
-public class Main {
+import javax.swing.JFrame;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+public class Main extends JFrame{
+
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try
+				{
+					Main frame = new Main();
+					frame.setVisible(true);
+				}
+				catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	// Constructor for Main
+	// Creating the JFrame Main
+	public Main(){
+		
+		this.setSize(250,250);
+		this.setLocationRelativeTo(null); // This line of code will center the program
+		this.setResizable(false); // Disables the wide screen button
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE); // Without this code the program will still run in the background even after there user clicks the red X
 	}
 
 }
